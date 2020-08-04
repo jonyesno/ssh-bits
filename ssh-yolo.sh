14f729eea8d67e1e3b9a882925bbf8e632d44cfb
Jon Stuart <jon@zikomo.xyz>
2020-08-04 10:03:54 +1000

    ssh-yolo - use a bash exit handler to always run ssh-undefeat
    upon ^C, for example

--- ssh-yolo.sh
+++ ssh-yolo.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 AGENT=$1
 
-notify() {
-  terminal-notifier -title SSH -subtitle "¯\_(ツ)_/¯" -group ssh-askpass -message "$@" -sound Tink
+unyolo() {
+  notify "unyolo ${AGENT}" || true
+  ssh-undefeat.sh ${AGENT} || true
 }
+trap unyolo ERR EXIT
 
+notify() {
+  terminal-notifier -title SSH -subtitle "¯\_(ツ)_/¯" -group ssh-askpass -message "$@" -sound Tink >/dev/null 2>&1
+}
 
 if [ -z "${AGENT}" ] ; then
   notify "no agent to yolo"
@@ -15,5 +20,4 @@ fi
 notify "yolo ${AGENT}"
 ssh-defeat.sh $1
 sleep 600
-notify "unyolo ${AGENT}"
-ssh-undefeat.sh $1
+# ssh-undefeat via exit handler
