513a3e8c04c0162d0e8e85b71c0def60ece115a4
Jon Stuart <jon@zikomo.xyz>
2023-04-19 15:57:11 +1000

    replace Makefile with install.sh

--- Makefile
+++
@@ -1,6 +0,0 @@
-install:
-	mkdir -p ~/bin
-	@chmod a+x *.sh *.rb
-	for _b in *.sh *.rb ; do ln -nsf $(CURDIR)/$${_b} ~/bin/$${_b} ; done
-	@echo
-	@echo scripts symlinked into ~/bin - make sure PATH sees that
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+TARGETS="
+	clear-agents
+	create-ssh-agent-for-key
+	load-key
+	ssh-agent-auth
+	ssh-agent-loader
+	ssh-askpass-decline
+	ssh-askpass
+	ssh-decline
+	ssh-defeat
+	ssh-undefeat
+	ssh-with-lookup
+	ssh-yolo
+"
+
+mkdir -p ~/bin
+for target in ${TARGETS} ; do
+	chmod a+x "${target}"
+	ln -nsf "${DIR}/${target}" "${HOME}/bin/${target}"
+	ls -l "${HOME}/bin/${target}"
+done
+
